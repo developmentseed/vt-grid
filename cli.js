6ff8e48d95de5b45b2473051c896710f80e91946
Anand Thakker <vestibule@anandthakker.net>
2015-07-31 23:15:22 -0400

    CLI defaults based on mbtiles metadata

--- cli.js
+++ cli.js
@@ -9,11 +9,9 @@ var argv = require('yargs')
   .usage('$0 data.mbtiles --minzoom 7 --basezoom 12 --layer census [--gridsize 4] --fields \'density:areaWeightedMean(density)\' \'zones:count()\'')
   .demand(1)
   .default('minzoom', 1)
-  .demand('basezoom')
-  .demand('layer')
   .array('fields')
   .demand('fields')
-  .default('gridsize', 4)
+  .default('gridsize', 1024)
   .describe('basezoom', 'The zoom level *above* which to start building (data should already exist at z-basezoom).')
   .describe('minzoom', 'The lowest zoom level at which to build the grid.')
   .help('h')
@@ -43,20 +41,29 @@ argv.progress = function (tiles, total, features, nextTile) {
 
 var mbtiles = new MBTiles('mbtiles://' + input, function (err) {
   if (err) { throw err }
-  argv.layers = {}
-  argv.layers[argv.layer] = { }
-  argv.fields.forEach(function (field) {
-    // outField:func(inField)
-    var match = /([^:]+):([^\(]+)\((.*)\)/.exec(field)
-    var outField = match[1]
-    var fn = match[2]
-    var inField = match[3]
-    argv.layers[argv.layer][outField] = aggregate[fn](inField)
-  })
-
-  grid(mbtiles, argv, function (err) {
-    bar.terminate()
+  mbtiles.getInfo(function (err, info) {
     if (err) { throw err }
-    console.log('Finished!')
+    if (!argv.layer) {
+      argv.layer = info.vector_layers[0].id
+    }
+    if (!argv.basezoom) {
+      argv.basezoom = info.minzoom
+    }
+    argv.layers = {}
+    argv.layers[argv.layer] = { }
+    argv.fields.forEach(function (field) {
+      // outField:func(inField)
+      var match = /([^:]+):([^\(]+)\((.*)\)/.exec(field)
+      var outField = match[1]
+      var fn = match[2]
+      var inField = match[3]
+      argv.layers[argv.layer][outField] = aggregate[fn](inField)
+    })
+
+    grid(mbtiles, argv, function (err) {
+      bar.terminate()
+      if (err) { throw err }
+      console.log('Finished!')
+    })
   })
 })
