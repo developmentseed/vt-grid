deb74f74cfb86f7696f962b7c4971a11f7efc814
Anand Thakker <vestibule@anandthakker.net>
2015-10-14 11:03:54 -0400

    Reoganize test a bit

--- test/basic.js
+++ test/basic.js
@@ -3,63 +3,56 @@ var path = require('path')
 var test = require('tap').test
 var tmp = require('tmp')
 var vtgeojson = require('vt-geojson')
+var aggregations = require('geojson-polygon-aggregate')
 var MBTiles = require('mbtiles')
-var vtgrid = require('../')
+var grid = require('../lib/grid')
 
 test('basic aggregations', function (t) {
-  var output = tmp.tmpNameSync({postfix: '.mbtiles'})
-  vtgrid({
-    input: 'mbtiles://' + path.resolve(__dirname, 'fixture', 'dc.mbtiles'),
-    output: 'mbtiles://' + path.resolve(output),
-    minzoom: 1,
-    gridsize: 64,
-    aggregations: {
-      'dc': {
-        'data': 'sum'
-      }
-    }
-  }, function (err) {
+  var input = 'mbtiles://' + path.resolve(__dirname, 'fixture', 'dc.mbtiles')
+  var output = 'mbtiles://' + path.resolve(tmp.tmpNameSync({postfix: '.mbtiles'}))
+
+  var job = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'fixture/job.json')))
+  job.aggregations = { dc: { data: aggregations.sum('data') } }
+  job.postAggregations = {}
+
+  load(input, output, function (err, inp, out) {
     t.error(err)
+    grid(out, inp, job, function (err, progress, nextlevel) {
+      t.error(err)
 
-    var expected = fs.readFileSync(__dirname + '/fixture/dc.z12-grid-quadkeys.txt')
-      .toString()
-      .split('\n')
+      var expected = fs.readFileSync(__dirname + '/fixture/dc.z12-grid-quadkeys.txt')
+        .toString()
+        .split('\n')
 
-    var results = {}
-    vtgeojson('mbtiles://' + output, {
-      minzoom: 12,
-      bounds: JSON.parse(fs.readFileSync(__dirname + '/fixture/dc.geojson'))
-    })
-    .on('data', function (feature) {
-      results[feature.properties._quadKey] = feature.properties.data
-    })
-    .on('end', function () {
-      console.log(output)
-      for (var key in results) {
-        if (expected.indexOf(key) >= 0) {
-          t.ok(results[key] > 0)
-        } else {
-          t.ok(results[key] === 0 || !results[key])
+      var results = {}
+      vtgeojson(output, {
+        minzoom: 12,
+        bounds: JSON.parse(fs.readFileSync(__dirname + '/fixture/dc.geojson'))
+      })
+      .on('data', function (feature) {
+        results[feature.properties._quadKey] = feature.properties.data
+      })
+      .on('end', function () {
+        for (var key in results) {
+          if (expected.indexOf(key) >= 0) {
+            t.ok(results[key] > 0)
+          } else {
+            t.ok(results[key] === 0 || !results[key])
+          }
         }
-      }
-
-      getInfo(output, function (err, info) {
-        t.error(err)
-        var expectedJSON = fs.readFileSync(__dirname + '/fixture/dc.tilejson.json')
-        expectedJSON = JSON.parse(expectedJSON)
-        delete info.id
-        delete info.basename
-        delete info.filesize
-        t.same(info, expectedJSON, 'tilejson metadata')
         t.end()
       })
     })
   })
 })
 
-function getInfo (file, callback) {
-  var mbtiles = new MBTiles('mbtiles://' + file, function (err) {
+function load (input, output, callback) {
+  var inp = new MBTiles(input, function (err) {
     if (err) return callback(err)
-    mbtiles.getInfo(callback)
+    var out = new MBTiles(output, function (err) {
+      if (err) return callback(err)
+      callback(null, inp, out)
+    })
   })
 }
+
---
+++ test/index.js
@@ -0,0 +1,66 @@
+var fs = require('fs-extra')
+var path = require('path')
+var test = require('tap').test
+var tmp = require('tmp')
+var vtgeojson = require('vt-geojson')
+var MBTiles = require('mbtiles')
+var vtgrid = require('../')
+
+test('main module', function (t) {
+  var output = tmp.tmpNameSync({postfix: '.mbtiles'})
+  vtgrid({
+    input: 'mbtiles://' + path.resolve(__dirname, 'fixture', 'dc.mbtiles'),
+    output: 'mbtiles://' + path.resolve(output),
+    minzoom: 1,
+    gridsize: 64,
+    jobs: 1,
+    aggregations: {
+      'dc': {
+        'data': 'sum'
+      }
+    }
+  }, function (err) {
+    t.error(err)
+
+    var expected = fs.readFileSync(__dirname + '/fixture/dc.z12-grid-quadkeys.txt')
+      .toString()
+      .split('\n')
+
+    var results = {}
+    vtgeojson('mbtiles://' + output, {
+      minzoom: 12,
+      bounds: JSON.parse(fs.readFileSync(__dirname + '/fixture/dc.geojson'))
+    })
+    .on('data', function (feature) {
+      results[feature.properties._quadKey] = feature.properties.data
+    })
+    .on('end', function () {
+      console.log(output)
+      for (var key in results) {
+        if (expected.indexOf(key) >= 0) {
+          t.ok(results[key] > 0)
+        } else {
+          t.ok(results[key] === 0 || !results[key])
+        }
+      }
+
+      getInfo(output, function (err, info) {
+        t.error(err)
+        var expectedJSON = fs.readFileSync(__dirname + '/fixture/dc.tilejson.json')
+        expectedJSON = JSON.parse(expectedJSON)
+        delete info.id
+        delete info.basename
+        delete info.filesize
+        t.same(info, expectedJSON, 'tilejson metadata')
+        t.end()
+      })
+    })
+  })
+})
+
+function getInfo (file, callback) {
+  var mbtiles = new MBTiles('mbtiles://' + file, function (err) {
+    if (err) return callback(err)
+    mbtiles.getInfo(callback)
+  })
+}
